42db361a81eb42dfaa637a4cac3cdda8a8e87b28
misterKumar <120304503+misterKumar@users.noreply.github.com>
2023-08-15 14:23:20 +0530

    check2

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -11,13 +11,13 @@ const Tour = ({ tour, removeTour }) => {
           <h4>{tour.name}</h4>
           <h4 className="tour-price">${tour.price}</h4>
         </div>
-        <p>
+        <p id='tour-item-para-rec6d6T3q5EBIdCfD'>
           {readMore ? tour.info : `${tour.info.substring(0, 200)}...`}
           <button onClick={() => setReadMore(!readMore)}>
             {readMore ? 'See Less' : 'Read More'}
           </button>
         </p>
-        <button className="delete-btn" onClick={() => removeTour(tour.id)}>
+        <button className="delete-btn" id="delete-btn-rec6d6T3q5EBIdCfD" onClick={() => removeTour(tour.id)}>
           Not Interested
         </button>
       </footer>
