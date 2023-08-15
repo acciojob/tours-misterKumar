52b1e177d21b59261696fd8faf5cbd2e1a12ebf7
misterKumar <120304503+misterKumar@users.noreply.github.com>
2023-08-15 13:32:16 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import Tour from './Tour';
+import tourData from './TourData'; // Import the predefined tour data
 
 const App = () => {
-    return(
-      <main id="main">
-      </main>
-    )
-}
+  const [loading, setLoading] = useState(false);
+  const [tours, setTours] = useState(tourData); // Use the predefined tour data
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const refreshTours = () => {
+    setLoading(false);
+    setTours(tourData); // Reset to the predefined tour data
+  };
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="main">
+        <h2 className="title">Loading...</h2>
+      </div>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <div className="main">
+        <h2 className="title">No more tours</h2>
+        <button className="btn" onClick={refreshTours}>
+          Refresh
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="main">
+      <h2 className="title">Our Tours</h2>
+      <div>
+        {tours.map((tour) => (
+          <Tour key={tour.id} tour={tour} removeTour={removeTour} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
 export default App;
